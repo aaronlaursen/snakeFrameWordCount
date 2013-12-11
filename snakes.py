e48996655f1ac3afe488b1d478f757562f57597b
aaronlaursen <aaronlaursen@vm-2.aaronlaursen.koding.kd.io>
2013-12-11 23:24:16 +0000

    update and cleanup

--- snakes.py
+++ snakes.py
@@ -3,11 +3,14 @@
 def work_gen(unit_id):
 	return (lambda x: True)
 
+#return iterator of dpendencies for task unit_id
 def deps_gen(unit_id):
 	return []
 
+#run when all tasks completed
 def task_final():
     return
 
+#run when a task fails, before cleanup
 def task_fail():
     return
